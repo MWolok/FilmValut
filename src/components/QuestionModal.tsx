852f364c58534a1090b6e72e34cb41d5836cf044
Mateusz <wmateusz993@gmail.com>
2023-08-03 18:32:07 +0200

    Quiz work

--- src/components/QuestionModal.tsx
+++ src/components/QuestionModal.tsx
@@ -1,37 +1,99 @@
+import { useEffect, useState } from "react";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
+import { Question } from "../model/QuizQuestionModel";
+
+const questions: Question[] = [
+	{
+		id: 1,
+		movie: "The Dark Knight",
+		question: "Who portrayed Batman/Bruce Wayne in 'The Dark Knight'?",
+		answerA: "Christian Bale",
+		answerB: "Michael Keaton",
+		answerC: "Ben Affleck",
+	},
+	{
+		id: 2,
+		movie: "The Dark Knight",
+		question:
+			"What is the name of the criminal mastermind who becomes the Joker?",
+		answerA: "Harvey Dent",
+		answerB: "Oswald Cobblepot",
+		answerC: "The Riddler",
+	},
+	{
+		id: 3,
+		movie: "The Dark Knight",
+		question:
+			"Which actor won an Oscar for their role as the Joker in 'The Dark Knight'?",
+		answerA: "Heath Ledger",
+		answerB: "Jack Nicholson",
+		answerC: "Joaquin Phoenix",
+	},
+];
 
 const QuestionModal = (props: any) => {
+	const [num,setNum] = useState(0);
+	const [curentQuestion, setCurentQuestion] = useState<Question>(questions[num]);
+	const [selectedAnswer, setSelectedAnswer] = useState("");
+
+	const handleCheckboxChange = (answer:string) => {
+		setSelectedAnswer(answer);
+	};
+
+const nextQuestion = ()=>{
+	setNum((prevNum) => prevNum+1);
+	
+}
+useEffect(()=>{
+	setCurentQuestion(questions[num])
+},[num])
+
 	return (
 		<>
 			<Modal {...props} aria-labelledby="contained-modal-title-vcenter">
 				<Modal.Header closeButton>
 					<Modal.Title id="contained-modal-title-vcenter">
-						Movie Title
+						{curentQuestion.movie}
 					</Modal.Title>
 				</Modal.Header>
 				<Modal.Body className="grid-example">
 					<Container>
 						<Row>
 							<Col xs={12} md={12}>
-								<p>Question ?</p>
+								<p>{curentQuestion.question}</p>
 							</Col>
 						</Row>
 
 						<Row>
 							<Col xs={4} md={4}>
-								<input type="checkbox">A</input>
+								<input
+									type="checkbox"
+									onChange={() => handleCheckboxChange(questions[num].answerA)}
+									checked={selectedAnswer === questions[num].answerA}
+								/>
+								{questions[num].answerA}
 							</Col>
 							<Col xs={4} md={4}>
-								<input type="checkbox">B</input>
+							<input
+									type="checkbox"
+									onChange={() => handleCheckboxChange(questions[num].answerB)}
+									checked={selectedAnswer === questions[num].answerB}
+								/>
+								{questions[num].answerB}
 							</Col>
 							<Col xs={4} md={4}>
-								<input type="checkbox">C</input>
+							<input
+									type="checkbox"
+									onChange={() => handleCheckboxChange(questions[num].answerC)}
+									checked={selectedAnswer === questions[num].answerC}
+								/>
+								{questions[num].answerC}
 							</Col>
 						</Row>
 					</Container>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button onClick={props.onHide}>Next</Button>
+					<Button onClick={nextQuestion}>Next</Button>
 					<Button onClick={props.onHide}>Close</Button>
 				</Modal.Footer>
 			</Modal>
--- src/components/UserTvSeriesCard.tsx
+++ src/components/UserTvSeriesCard.tsx
@@ -2,43 +2,38 @@ import { ListGroup } from "react-bootstrap";
 import { MovieModel } from "../model/MovieModel.";
 import { TvSeries } from "../model/TvSeriesModel";
 
-
-
 interface Props {
-    series:TvSeries,
-    tvSeriesDelate:(id:number)=>void
+	series: TvSeries;
+	tvSeriesDelate: (id: number) => void;
 }
-const UserTvSeriesCard = (props:TvSeries) => {
-
-
-
-return(<><div className="card">
-{/* <!-- Card image --> */}
-<div className="view overlay">
-<img
+const UserTvSeriesCard = (props: TvSeries) => {
+	return (
+		<>
+			<div className="card">
+				<div className="view overlay">
+					<img
 						className="card-img-top"
 						src={`https://image.tmdb.org/t/p/w500/${props.posterPath}`}
 						alt="Card image cap"
 					/>
-    <a href="#!">
-        <div className="mask rgba-white-slight"></div>
-    </a>
-</div>
+					<a href="#!">
+						<div className="mask rgba-white-slight"></div>
+					</a>
+				</div>
 
-{/* <!-- Card content --> */}
-<div className="card-body">
-    {/* <!-- Title --> */}
-    <h4 className="card-title">{props.name}</h4>
-    {/* <!-- Text --> */}
-    <p className="card-text">
-      Episodes:  {props.episodes}
-    </p>
-    <ListGroup className="list-group-flush">
-        <ListGroup.Item>First air date: {props.firstAirDate}</ListGroup.Item>
-        <ListGroup.Item>Number of Seasons: {props.number_of_seasons}</ListGroup.Item>
-        <ListGroup.Item>
-						{props.vodPlatforms.map((v) => (
-						
+				<div className="card-body">
+					<h4 className="card-title">{props.name}</h4>
+
+					<p className="card-text">Episodes: {props.episodes}</p>
+					<ListGroup className="list-group-flush">
+						<ListGroup.Item>
+							First air date: {props.firstAirDate}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							Number of Seasons: {props.number_of_seasons}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							{props.vodPlatforms.map((v) => (
 								<a rel="noreferrer" target="_blank" href={v.vodURL}>
 									<img
 										src={`https://image.tmdb.org/t/p/w500/${v.logoPath}`}
@@ -47,14 +42,13 @@ return(<><div className="card">
 										width="50"
 									/>
 								</a>
-							
-						))}
+							))}
 						</ListGroup.Item>
-    </ListGroup>
-    {/* <!-- Button --> */}
-
-</div>
-</div></>)
-
+					</ListGroup>
+					{/* <!-- Button --> */}
+				</div>
+			</div>
+		</>
+	);
 };
 export default UserTvSeriesCard;
